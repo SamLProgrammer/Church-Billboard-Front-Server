68d0c4ec0484c7ba8865c276e207229764a13f09
Samuel Felipe Lopez Roncancio <samuel.lopez@tengo.co>
2023-10-19 11:04:07 -0500

    first push

---
+++ .history/index_20231019101332.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+// Define your other server-side routes and APIs here if needed
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
