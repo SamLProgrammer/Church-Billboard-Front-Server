75c848e21bb546929895b24a04fcb9209b163af2
Samuel Felipe Lopez Roncancio <samuel.lopez@tengo.co>
2023-11-04 17:41:29 -0500

    fixing paths

--- .history/index_20231104174024.js
+++ .history/index_20231104174024.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, '..', 'church_billboard_front_app', 'dist')));
+app.use(express.static(path.join(__dirname, '..', 'dist', 'church_billboard_front_app')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'church_billboard_front_app', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'dist', 'church_billboard_front_app', 'index.html'));
 });
 
 // Define your other server-side routes and APIs here if needed
